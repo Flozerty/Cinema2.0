fb0a62aa86367e31c8fa4556a842886be2d87056
Flozerty <floris.louerat@gmail.com>
2024-04-04 20:15:25 +0200

    + subtitle class

--- view/filmCard.php
+++ view/filmCard.php
@@ -1,8 +1,10 @@
-<figure>
-  <img src="<?= $film["affiche"] ?>" alt="Affiche du film <?= $film["nom_film"] ?>">
-  <figcaption><?= $film["nom_film"] ?></figcaption>
-  <div class="note">
-    <?= $film["note"] ?>
-    <i class="fa-solid fa-star"></i>
-  </div>
-</figure>
+<a href="index.php?action=detailFilm&id=<?= $film["id_film"] ?>">
+  <figure>
+    <img src="<?= $film["affiche"] ?>" alt="Affiche du film <?= $film["nom_film"] ?>">
+    <figcaption class="subtitle"><?= $film["nom_film"] ?></figcaption>
+    <div class="note">
+      <?= $film["note"] ?>
+      <i class="fa-solid fa-star"></i>
+    </div>
+  </figure>
+</a>
