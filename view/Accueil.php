d88bc0c1c167bbf3333a86a574ebb1c7f65de91b
Flozerty <floris.louerat@gmail.com>
2024-04-05 16:00:02 +0200

    + carrousel wrapper

--- view/Accueil.php
+++ view/Accueil.php
@@ -126,11 +126,13 @@ $filmsSf = $requeteSF->fetchAll();
       <div class="carroussel">
         <i class="fa-solid fa-circle-arrow-left arrow arrow-left"></i>
 
-        <div class="cards-container">
-          <?php foreach( $films as $film) {
+        <div class="carroussel-wrapper">
+          <div class="cards-container">
+            <?php foreach( $films as $film) {
 
             require "templates/filmCard.php";
          } ?>
+          </div>
         </div>
 
         <i class="fa-solid fa-circle-arrow-right arrow arrow-right"></i>
