6cb4b7d432715a9b51074007491cc8d207e0ee55
Flozerty <floris.louerat@gmail.com>
2024-04-10 16:20:03 +0200

    new font

--- templates/template.php
+++ templates/template.php
@@ -8,6 +8,12 @@
     content="PDO-Cinéma, un projet de wiki concernant le cinéma, recensant les acteurs, réalisateurs, et rôles en tout genres des films du monde entier! :o">
   <title><?= $titre ?></title>
 
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=M+PLUS+1+Code:wght@100..700&display=swap"
+    rel="stylesheet">
+
   <script src="https://kit.fontawesome.com/7252ea4d54.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="public/css/styles.css">
 </head>
@@ -75,6 +81,7 @@
 
   <script src="public/js/menuBurger.js"></script>
   <script src="public/js/carrousel.js"></script>
+  <script src="public/js/ajouterFilm.js"></script>
 </body>
 
 </html>
