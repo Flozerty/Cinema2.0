161842890c75cba0c38f2d8c8e90107caeab4046
Flozerty <floris.louerat@gmail.com>
2024-04-10 15:08:48 +0200

    + meta description

--- templates/template.php
+++ templates/template.php
@@ -4,6 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description"
+    content="PDO-Cinéma, un projet de wiki concernant le cinéma, recensant les acteurs, réalisateurs, et rôles en tout genres des films du monde entier! :o">
   <title><?= $titre ?></title>
 
   <script src="https://kit.fontawesome.com/7252ea4d54.js" crossorigin="anonymous"></script>
