fbba0c3c352ecbaf272db643eedc800b83d40b1b
Flozerty <floris.louerat@gmail.com>
2024-04-17 11:48:40 +0200

    notifications

---
+++ public/js/notif.js
@@ -0,0 +1,56 @@
+let notifDiv = document.querySelectorAll('.notification');
+
+let timeouts = [];
+
+affichageElements()
+
+// affichage des elements
+
+function affichageElements() {
+  let i = 0;
+
+  notifDiv.forEach(element => {
+    const message = element.querySelector('p');
+    const xmark = element.querySelector('i');
+
+    xmark.addEventListener('click', () => {
+      element.parentNode.removeChild(element);
+      // notifDiv.forEach(el => {
+      //   clearTimeout(timeout);
+      // });
+
+      timeouts.forEach(timeout => {
+        clearTimeout(timeout);
+      });
+
+      redimensionner();
+    })
+
+    element.style.bottom = 50 + 70 * i + "px";
+
+    let timeout = setTimeout(() => {
+      element.parentNode.removeChild(element);
+    }, 1500 * ((notifDiv.length - i) + 1));
+    timeouts.push(timeout)
+
+    i++;
+  })
+}
+// const notifs = setInterval(notifDisplay, 2500)
+
+// function notifDisplay() {
+//   element = notifDiv[i];
+
+//   if (!element) {
+//     clearInterval()
+//   }
+//   setTimeout(() => {
+//     element.parentNode.removeChild(element)
+//   }, 2500);
+// }
+
+function redimensionner() {
+  notifDiv = document.querySelectorAll('.notification');
+  console.log(notifDiv)
+  affichageElements()
+}
